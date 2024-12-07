81dba66f35e89e565f69fee71301f47740fcfb65
RyneeeJ <imrynegandia@gmail.com>
2024-12-07 17:10:50 +0800

    set up project color palette

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,20 @@ export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   darkMode: "selector",
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        gray: {
+          darkest: "#222",
+          dark: "#3a3a3a",
+          medium: "#888",
+          light: "#d9d9d9",
+        },
+        offWhite: "#f6f6f6",
+        green: {
+          brand: "#c6de41",
+        },
+      },
+    },
   },
   plugins: [],
 };
