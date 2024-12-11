908305695ec060cfd6b58295796b1217fedb867d
RyneeeJ <imrynegandia@gmail.com>
2024-12-12 00:48:15 +0800

    Fix core skills list alignment

--- src/components/SkillsListItem.jsx
+++ src/components/SkillsListItem.jsx
@@ -6,7 +6,7 @@ function SkillsListItem({ name, iconPath }) {
       whileInView={{ opacity: 1, y: 0 }}
       transition={{ duration: 0.5 }}
       viewport={{ margin: "-80px", once: true }}
-      className="flex h-full w-full max-w-72 flex-col items-center justify-center gap-4 rounded-md bg-gray-light px-4 py-4 odd:place-self-end min-[500px]:flex-row md:py-6 lg:place-self-center lg:odd:place-self-center dark:bg-gray-dark"
+      className="flex h-full w-full max-w-72 flex-col items-center gap-4 rounded-md bg-gray-light px-4 py-4 odd:place-self-end min-[500px]:flex-row min-[500px]:px-6 md:px-8 md:py-6 lg:place-self-center lg:odd:place-self-center dark:bg-gray-dark"
     >
       <div className="aspect-square h-12">
         <img src={iconPath} alt={name} />
